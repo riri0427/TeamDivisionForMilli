ea5511012010ac17af52dcd610ec57733e498cf1
riri0427 <riri190427@gmail.com>
2022-07-16 16:03:12 +0900

    Initial Commit

---
+++ TeamDivisionForMilli/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TeamDivisionForMilli
+//
+//  Created by riri0427 on 2022/07/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
